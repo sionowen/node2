95acf5f56909de35046bb4df0a028cef0e67e394
sionowen <sionowen34@gmail.com>
2015-02-19 21:13:28 -0700

    Achieved rudimentary setup of a server

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+var fileContents = fs.readFileSync('data.txt');
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
@@ -9,7 +11,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
 	res.render('index');
+	res.header('Content-type', 'text/html');
+	res.send('fileContents');
 });
+
 
 var server = app.listen(4406, function() {
 	console.log('Express server listening on port ' + server.address().port);
