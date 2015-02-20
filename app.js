0a7ddc51dd7c4d2365501e8cec12ea90a252ecd8
SionOwen <sionowen34@gmail.com>
2015-02-20 10:59:27 -0700

    made server deliver content asynchronusly

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
-var fileContents = fs.readFileSync('data.txt');
+fs.readFile('data.txt', function(err, data){
+	res.header('Content-type', 'text/html');
+	res.send(fileContents);
+});
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
-	res.render('index');
-	res.header('Content-type', 'text/html');
-	res.send('fileContents');
 });
 
 
